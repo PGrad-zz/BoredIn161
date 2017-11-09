8d19fd711b261d6d93ffe729eb1d5a5c80776db2
PGrad <paulgrad3rd@gmail.com>
2017-11-08 19:45:12 -0800

    Fun with Javascript

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ function CelestialObject(geometry = undefined, material = undefined, mesh = unde
 		return this.mesh.rotation;
 	}
 }
+function Star(geometry = undefined, material = undefined, mesh = undefined, revolution = undefined, rotation = undefined, light = undefined) {
+	CelestialObject.call(this, geometry, material, mesh, revolution, rotation);
+	this.light = light;
+}
 function Game(scene = undefined, camera = undefined, renderer = undefined) {
 	this.scene = scene;
 	this.camera = camera;
@@ -25,6 +29,7 @@ game.renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(game.renderer.domElement);
 let loader = new THREE.TextureLoader();
 let sun = new CelestialObject();
+let pair_sun = new CelestialObject();
 let earth = new CelestialObject();
 let moon = new CelestialObject();
 loader.load('./assets/water.jpg', skip);
@@ -35,37 +40,37 @@ function skip(texture) {
 function main(texture) {
 	moon.material = new THREE.MeshLambertMaterial({map: texture});
 	sun.material = new THREE.MeshLambertMaterial({emissive: 0xffffff});
-	pair_sun_material = new THREE.MeshLambertMaterial({emissive: 0xff0000});
+	pair_sun.material = new THREE.MeshLambertMaterial({emissive: 0xff0000});
 	sun.geometry = new THREE.SphereGeometry(12);
-	let pair_sun_geometry = new THREE.SphereGeometry(20);
+	pair_sun.geometry = new THREE.SphereGeometry(20);
 	earth.geometry = new THREE.SphereGeometry(10);
 	moon.geometry = new THREE.SphereGeometry(1);
 	sun.mesh = new THREE.Mesh(sun.geometry, sun.material);
 	sun.revolution = Math.PI / 100;
-	let pair_sun = new THREE.Mesh(pair_sun_geometry, pair_sun_material);
+	pair_sun.mesh = new THREE.Mesh(pair_sun.geometry, pair_sun.material);
 	earth.mesh = new THREE.Mesh(earth.geometry, earth.material);
 	earth.rotation = Math.PI / 150;
 	earth.revolution = Math.PI / 400;
 	moon.mesh = new THREE.Mesh(moon.geometry, moon.material);
 	moon.revolution = Math.PI / 200;
 	sun.position().set(20,0,0);
-	pair_sun.position.set(-20,0,0);
+	pair_sun.position().set(-20,0,0);
 	earth.position().set(150,0,150);
 	moon_earth_dist = new THREE.Vector3(20,0,20);
 	moon.position().addVectors(earth.position(), moon_earth_dist);
 	game.scene.add(sun.mesh);
-	game.scene.add(pair_sun);
+	game.scene.add(pair_sun.mesh);
 	game.scene.add(moon.mesh);
 	game.scene.add(earth.mesh);
 	game.camera.position.set(0,100,300);
 	game.camera.lookAt(0,0,0);
 	let center = new THREE.Vector3(0,0,0);
-	let sun_light = new THREE.PointLight(0xdd90a5, 5, 0);
-	sun_light.position.set(20, 0, 0);
-	game.scene.add(sun_light);
-	let pair_sun_light = new THREE.PointLight(0xff0000, 5, 0);
-	pair_sun_light.position.set(-20, 0, 0);
-	game.scene.add(pair_sun_light);
+	sun.light = new THREE.PointLight(0xdd90a5, 5, 0);
+	sun.light.position.set(20, 0, 0);
+	game.scene.add(sun.light);
+	pair_sun.light = new THREE.PointLight(0xff0000, 5, 0);
+	pair_sun.light.position.set(-20, 0, 0);
+	game.scene.add(pair_sun.light);
 	let revAxis = new THREE.Vector3(0,1,0);
 	let rotAxis = new THREE.Vector3(-0.53283302033,-0.84622040417,0);
 	let rotation = new THREE.Quaternion();
@@ -73,13 +78,13 @@ function main(texture) {
 	function animate() {
 		requestAnimationFrame( animate );
 		sun.get_rotation().y += 0.01;
-		pair_sun.rotation.y += 0.01;
+		pair_sun.get_rotation().y += 0.01;
 		moon.get_rotation().y += 0.03;
 		earth.mesh.applyQuaternion(rotation);
 		rotateAboutPoint(sun.mesh, center, revAxis, sun.revolution);
-		rotateAboutPoint(sun_light, center, revAxis, sun.revolution);
-		rotateAboutPoint(pair_sun, center, revAxis, sun.revolution);
-		rotateAboutPoint(pair_sun_light, center, revAxis, sun.revolution);
+		rotateAboutPoint(sun.light, center, revAxis, sun.revolution);
+		rotateAboutPoint(pair_sun.mesh, center, revAxis, sun.revolution);
+		rotateAboutPoint(pair_sun.light, center, revAxis, sun.revolution);
 		rotateAboutPoint(earth.mesh, center, revAxis, earth.revolution);
 		rotateAboutPoint(moon.mesh, center, revAxis, earth.revolution);
 		rotateAboutPoint(moon.mesh, earth.position(), revAxis, moon.revolution);
